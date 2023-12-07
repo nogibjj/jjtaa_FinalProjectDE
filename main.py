0f139594229b789cfef67d51c1344dffc6111ca4
Jeremy Tan <36715338+jeremymtan@users.noreply.github.com>
2023-12-07 03:08:35 -0500

    sample

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-"""
-Main cli or app entry point
-"""
+from dotenv import load_dotenv
+import os
+from flask import Flask, render_template
+from databricks import sql
 
-from mylib.calculator import add
-import click
+load_dotenv()
 
-#var=1;var=2
+app = Flask(__name__)
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+# Azure Databricks API token
+DATABRICKS = os.getenv("DATABRICKS")
+DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
+DATABRICKS_PATH = os.getenv("DATABRICKS_PATH")
+
+
+# Function to get news from the warehouse
+def get_random_news():
+    """gets random news"""
+    server_h = DATABRICKS_HOST
+    access_token = DATABRICKS
+    http_path = DATABRICKS_PATH
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        c.execute("SELECT * FROM default.news ORDER BY RAND() LIMIT 5;")
+        result = c.fetchall()
+        return result
+
+
+@app.route("/")
+def index():
+    """simple index page"""
+    news = get_random_news()
+    return render_template("index.html", news=news)
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    app.run(debug=True)
