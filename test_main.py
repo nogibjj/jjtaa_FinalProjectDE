c7ec86d9d9940446d792389c27d885ba2124bd73
TianoRao <104114843+TianoRao@users.noreply.github.com>
2023-12-10 19:43:50 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,12 +9,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Azure Databricks API token
-DATABRICKS = os.getenv("DATABRICKS")
-DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
-DATABRICKS_PATH = os.getenv("DATABRICKS_PATH")
-
-FILESTORE_PATH = ""
-url = f"https://{DATABRICKS_HOST}/api/2.0"
+# DATABRICKS = os.getenv("DATABRICKS")
+# DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
+# DATABRICKS_PATH = os.getenv("DATABRICKS_PATH")
 
 # test html files
 def test_html_files(directory="templates/"):
@@ -26,22 +23,6 @@ def test_html_files(directory="templates/"):
         assert os.path.exists(file_path) and os.path.isfile(file_path)
 
 
-def check_filestore_path(path, headers):
-    try:
-        response = requests.get(url + f"/dbfs/get-status?path={path}", headers=headers)
-        response.raise_for_status()
-        return response.json()["path"] is not None
-    except Exception as e:
-        print(f"Error checking file path: {e}")
-        return False
-
-
-# Test if the specified FILESTORE_PATH exists
-def test_databricks():
-    headers = {"Authorization": f"Bearer {DATABRICKS}"}
-    assert check_filestore_path(FILESTORE_PATH, headers) is False
-
-
 if __name__ == "__main__":
     test_html_files()
-    test_databricks()
+    # test_databricks()
